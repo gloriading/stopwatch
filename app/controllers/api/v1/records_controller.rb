28706294025f2f87af4707509aaf1bbf9b330596
Gloria Ding <gloria.hc.ding@gmail.com>
2018-03-08 14:43:34 -0800

    added index, show, create actions to records_crotroller

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,2 +1,28 @@
-class Api::V1::RecordsController < ApplicationController
+class Api::V1::RecordsController < Api::ApplicationController
+
+
+  def index
+    records = Record.order(created_at: :desc)
+    render json: records
+  end
+
+  def show
+    record = Record.find params[:id]
+    render json: record
+  end
+
+  def create
+    record = Record.new record_params
+    if record.save
+      render json: record
+    else
+      render json: { error: record.errors.full_messages}
+    end
+  end
+
+	private
+  def record_params
+    params.require(:record).permit(:start_time, :end_time)
+  end
+
 end
