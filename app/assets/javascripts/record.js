7f3263cce93abbd76aa8f36604cea53577225202
Gloria Ding <gloria.hc.ding@gmail.com>
2018-03-09 16:48:24 -0800

    added headers on record.js

--- app/assets/javascripts/record.js
+++ app/assets/javascripts/record.js
@@ -2,13 +2,16 @@ const base_url = "http://localhost:3000";
 
 const records = {
   create (params) {
-  return fetch(
-    `${base_url}/api/v1/records`,
-    {
-      method: 'POST',
-      body: JSON.stringify(params)
-    }
-  )
-  .then(res => res.json());
+    return fetch(
+      `${base_url}/api/v1/records`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+      }
+    )
+    .then(res => res.json());
   }
 };
